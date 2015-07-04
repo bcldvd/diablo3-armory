fd8b2db425ca0c3d6ea3d88fcd8892b2094916c5
David Boclé <bcldvd@gmail.com>
2015-07-04 19:27:07 +0200

    Disallow empty battleTags + last search in localStorage

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -4,12 +4,20 @@ angular
     .module('starter.controllers')
     .controller('SearchCtrl', SearchCtrl);
 
-function SearchCtrl ($scope, $state, $filter) {
+function SearchCtrl ($scope, $state, $filter, localStorageService) {
     $scope.vm = {};
+    $scope.vm.battleTag = localStorageService.get('lastSearchedBattleTag');
 
-    $scope.DisplayProfile = function () {
+    $scope.DisplayProfile = function (battleTag) {
+
+        if (battleTag.trim() === '') {
+            return false;
+        }
+
+        localStorageService.set('lastSearchedBattleTag', battleTag);
+
         $state.go('app.heroes', {
-            battleTag: $filter('pound2Dash')($scope.vm.battleTag)
+            battleTag: $filter('pound2Dash')(battleTag)
         });
     }
 }
